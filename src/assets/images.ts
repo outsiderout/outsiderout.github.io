b9f3990d41a9c482daaae612eb6a3fb9322c4508
outsiderout <119620037+outsiderout@users.noreply.github.com>
2026-04-29 17:40:36 +0400

    Add files via upload

---
+++ src/assets/images.ts
@@ -0,0 +1,17 @@
+import heroHuman from '../../public/images/hero-human.png';
+import heroElf from '../../public/images/hero-elf.png';
+import heroOrc from '../../public/images/hero-orc.png';
+import heroDwarf from '../../public/images/hero-dwarf.png';
+import heroUndead from '../../public/images/hero-undead.png';
+import arenaBg from '../../public/images/arena-bg.png';
+import guildCastle from '../../public/images/guild-castle.png';
+
+export const IMAGES = {
+  heroHuman,
+  heroElf,
+  heroOrc,
+  heroDwarf,
+  heroUndead,
+  arenaBg,
+  guildCastle,
+} as const;
